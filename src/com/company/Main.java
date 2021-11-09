e890dd9b79a328363ad1fa12f4c674378d46f3dc
Aigera1 <aykeryma@yandex.ru>
2021-11-09 00:26:12 -0500

    car u asked for

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,12 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         car newcar=new car();
-        newcar.name="Ferarri";
-        newcar.color="blue";
-        newcar.doors=3;
-        newcar.engineCapacity="60000cc";
-        System.out.println(newcar);
+
+         newcar.name="Lambo";
+         newcar.engineCapacity="43678cc";
+         newcar.doors=7;
+         newcar.model="aventador";
+
     }
 
 }
